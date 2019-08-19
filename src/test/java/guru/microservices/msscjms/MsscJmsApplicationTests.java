d3c1f741e987b985db59b7a42de7fee193290dce
cvVal <cvvaldez_00@hotmail.com>
2019-08-18 23:58:22 -0500

    First commit

---
+++ src/test/java/guru/microservices/msscjms/MsscJmsApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.microservices.msscjms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsscJmsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
