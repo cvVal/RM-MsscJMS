848d2c184943d4fcd73f5804ee0ba23221d6372f
cvVal <cvvaldez_00@hotmail.com>
2019-08-31 17:19:43 -0500

    Add Jms implementation

--- src/main/java/guru/microservices/msscjms/listener/HelloMessageListener.java
+++ src/main/java/guru/microservices/msscjms/listener/HelloMessageListener.java
@@ -2,16 +2,24 @@ package guru.microservices.msscjms.listener;
 
 import guru.microservices.msscjms.config.JmsConfig;
 import guru.microservices.msscjms.model.HelloWorlMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorlMessage helloWorlMessage,
@@ -20,5 +28,17 @@ public class HelloMessageListener {
         System.out.println("I got a message!!");
 
         System.out.println(helloWorlMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_QUEUE)
+    public void listenForHello(@Payload HelloWorlMessage helloWorlMessage,
+                               @Headers MessageHeaders messageHeaders, Message message) throws JMSException {
+
+        HelloWorlMessage payloadMsg = HelloWorlMessage.builder()
+                .id(UUID.randomUUID())
+                .message(" World!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 }
--- src/main/java/guru/microservices/msscjms/sender/HelloSender.java
+++ src/main/java/guru/microservices/msscjms/sender/HelloSender.java
@@ -1,12 +1,18 @@
 package guru.microservices.msscjms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.microservices.msscjms.config.JmsConfig;
 import guru.microservices.msscjms.model.HelloWorlMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @Component
@@ -14,11 +20,10 @@ import java.util.UUID;
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
-
-        System.out.println("I'm sending a message");
 
         HelloWorlMessage message = HelloWorlMessage
                 .builder()
@@ -27,7 +32,33 @@ public class HelloSender {
                 .build();
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+    }
 
-        System.out.println("Message sent!");
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+
+        HelloWorlMessage message = HelloWorlMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+
+        Message receiveMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_QUEUE, session -> {
+
+            Message helloMessage;
+
+            try {
+
+                helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                helloMessage.setStringProperty("_type", "guru.microservices.msscjms.model.HelloWorlMessage");
+
+                return helloMessage;
+
+            } catch (JsonProcessingException e) {
+                throw new JMSException("boom");
+            }
+        });
+
+        System.out.println(receiveMessage.getBody(String.class));
     }
 }
